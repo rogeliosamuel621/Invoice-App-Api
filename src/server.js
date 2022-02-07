91b6610ef85ffd681a37a45f43480e55aaf0343f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-02-07 14:48:51 -0700

    add env variable

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3000
 
 server.use(middlewares)
 server.use('/api/json', router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
